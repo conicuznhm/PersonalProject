d1d382abca480790c46838e15f409e5961986534
conicuznhm <conicuzn@hotmail.com>
2023-03-13 18:06:33 +0700

    add feat admin

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,93 +1,82 @@
-const { validateSignUp, validateLogin } = require('../validators/auth-validators');
+const {
+  validateSignUp,
+  validateLogin,
+} = require("../validators/auth-validators");
 
-const { User } = require('../models');
-const bcrypt = require('bcryptjs');
-const errorFn = require('../utils/error-fn');
-const jwt = require('jsonwebtoken');
-
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+const errorFn = require("../utils/error-fn");
+const jwt = require("jsonwebtoken");
 
 exports.signUp = async (req, res, next) => {
-    try {
-        const value = validateSignUp(req.body);
-        const user = await User.findOne({
-            where: {
-                email: value.email || ''
-            }
-        })
+  try {
+    const value = validateSignUp(req.body);
+    const user = await User.findOne({
+      where: {
+        email: value.email || "",
+      },
+    });
 
-        if (user) {
-            // const err = new Error('Email is already in use');
-            // err.statusCode = 400;
-            // throw err;
-            errorFn('Email is already in use', 400)
-        }
+    if (user) {
+      // const err = new Error('Email is already in use');
+      // err.statusCode = 400;
+      // throw err;
+      errorFn("Email is already in use", 400);
+    }
 
-        value.password = await bcrypt.hash(value.password, 12)
-        await User.create(value)
+    value.password = await bcrypt.hash(value.password, 12);
+    await User.create(value);
 
-        res.status(201).json({ message: 'register success!!!' })
-    } catch (err) { next(err) }
-}
+    res.status(201).json({ message: "register success!!!" });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.login = async (req, res, next) => {
-    try {
-        const value = validateLogin(req.body);
+  try {
+    const value = validateLogin(req.body);
 
-        const user = await User.findOne({
-            where: {
-                email: value.email
-            }
-        })
-        if (!user) {
-            errorFn('Invalid email or password', 400);
-        }
-        const isMatch = await bcrypt.compare(value.password, user.password);
-        // console.log(isMatch)
-        if (!isMatch) {
-            errorFn('Invalid email or password', 400);
-        }
+    const user = await User.findOne({
+      where: {
+        email: value.email,
+      },
+    });
+    if (!user) {
+      errorFn("Invalid email or password", 400);
+    }
+    const isMatch = await bcrypt.compare(value.password, user.password);
+    // console.log(isMatch)
+    if (!isMatch) {
+      errorFn("Invalid email or password", 400);
+    }
 
-        const accessToken = jwt.sign(
-            {
-                id: user.id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email,
-                phone: user.phone,
-                profileImage: user.profileImage,
-                createdAt: user.createdAt,
-                updateAt: user.updateAt
-            },
-            process.env.JWT_SECRET_KEY,
-            { expiresIn: process.env.JWT_EXPIRES_IN }
-        )
-        // console.log(accessToken)
-        res.status(200).json({ accessToken })
-    } catch (err) { next(err) }
-}
+    const accessToken = jwt.sign(
+      {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        phone: user.phone,
+        role: user.role,
+        profileImage: user.profileImage,
+        createdAt: user.createdAt,
+        updateAt: user.updateAt,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: process.env.JWT_EXPIRES_IN },
+    );
+    // console.log(accessToken)
+    res.status(200).json({ accessToken });
+  } catch (err) {
+    next(err);
+  }
+};
 
-exports.getUser = (req, res, next) => {
-    res.status(200).json({ user: req.user })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+exports.getUser = async (req, res, next) => {
+  const user = await User.findOne({ where: { id: req.user.id } });
+  res.status(200).json({ user: req.user });
+};
 
 // if (user) {
 //     // const err = new Error('Email is already in use');
@@ -101,5 +90,3 @@ exports.getUser = (req, res, next) => {
 //     })
 //     throw new Error('delete complete')
 // }
-
-
